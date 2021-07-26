d1aafded22bbc28501ad4edaf1beda7818acde46
Sakil Jomadder <sakilorb@icloud.com>
2021-07-26 12:07:51 +0600

    previous post of main section completed

--- js/main.js
+++ js/main.js
@@ -1,37 +1,37 @@
 /* Navbar sticky */
-const header = document.querySelector("#header");
-const logoImg = document.querySelector(".header .row .left .logo .img");
-const profilePic = document.querySelector(".header .row .right .profile .img");
-const navLink = document.querySelectorAll(".header .row .center .nav .link");
-const scrollEventHandler = function () {
-  if (window.scrollY > 100) {
-    isSticky(header);
-    isSticky(logoImg);
-    isSticky(profilePic);
-    isSticky(navLink);
-  }
-  if (window.scrollY == 0) {
-    isSticky(header, false);
-    isSticky(logoImg, false);
-    isSticky(profilePic, false);
-    isSticky(navLink, false);
-  }
-};
-const isSticky = (element, add = true) => {
-  let check = (item = element) => {
-    add ? item.classList.add("sticky") : item.classList.remove("sticky");
-  };
-  if (element.length) {
-    element.forEach((item) => {
-      check(item);
-    });
-  } else {
-    check();
-  }
-};
+// const header = document.querySelector("#header");
+// const logoImg = document.querySelector(".header .row .left .logo .img");
+// const profilePic = document.querySelector(".header .row .right .profile .img");
+// const navLink = document.querySelectorAll(".header .row .center .nav .link");
+// const scrollEventHandler = function () {
+//   if (window.scrollY > 100) {
+//     isSticky(header);
+//     isSticky(logoImg);
+//     isSticky(profilePic);
+//     isSticky(navLink);
+//   }
+//   if (window.scrollY == 0) {
+//     isSticky(header, false);
+//     isSticky(logoImg, false);
+//     isSticky(profilePic, false);
+//     isSticky(navLink, false);
+//   }
+// };
+// const isSticky = (element, add = true) => {
+//   let check = (item = element) => {
+//     add ? item.classList.add("sticky") : item.classList.remove("sticky");
+//   };
+//   if (element.length) {
+//     element.forEach((item) => {
+//       check(item);
+//     });
+//   } else {
+//     check();
+//   }
+// };
 
-window.onscroll = () => {
-  scrollEventHandler();
-};
+// window.onscroll = () => {
+//   scrollEventHandler();
+// };
 
 /* Navbar sticky */
