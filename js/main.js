abe2b8d2109fcb78414732ff39cca9dcb7a053c7
Sakil Jomadder <sakilorb@icloud.com>
2021-07-30 11:08:20 +0600

    added darkTheme, completed with responsive issues, fixed few more issues

--- js/main.js
+++ js/main.js
@@ -1,46 +1,5 @@
 const body = document.body;
 const header = document.querySelector("#header");
-const main = document.querySelector("#main");
-const leftSidebar = document.querySelector("#leftSidebar");
-const rightSidebar = document.querySelector("#rightSidebar");
-const createNewPost = document.querySelector(".main .container .post");
-const publishedPost = document.querySelectorAll(".postPublished");
-/* Navbar sticky */
-// const logoImg = document.querySelector(".header .row .left .logo .img");
-// const profilePic = document.querySelector(".header .row .right .profile .img");
-// const navLink = document.querySelectorAll(".header .row .center .nav .link");
-// const scrollEventHandler = function () {
-//   if (window.scrollY > 100) {
-//     isSticky(header);
-//     isSticky(logoImg);
-//     isSticky(profilePic);
-//     isSticky(navLink);
-//   }
-//   if (window.scrollY == 0) {
-//     isSticky(header, false);
-//     isSticky(logoImg, false);
-//     isSticky(profilePic, false);
-//     isSticky(navLink, false);
-//   }
-// };
-// const isSticky = (element, add = true) => {
-//   let check = (item = element) => {
-//     add ? item.classList.add("sticky") : item.classList.remove("sticky");
-//   };
-//   if (element.length) {
-//     element.forEach((item) => {
-//       check(item);
-//     });
-//   } else {
-//     check();
-//   }
-// };
-
-// window.onscroll = () => {
-//   scrollEventHandler();
-// };
-
-/* Navbar sticky */
 
 /**   Setting Menu Toggler **/
 
@@ -58,22 +17,29 @@ body.onclick = function () {
 };
 /**   End: Setting Menu Toggler **/
 
-/**  Theme toggler  **/
-const themeChangeIcon = document.querySelector("#themeChangeIcon");
-themeChangeIcon.onclick = function () {
-  const dark = "dark";
-  this.title = this.title === "Light Theme" ? "Dark Theme" : "Light Theme";
-  this.classList.toggle(dark);
-  body.classList.toggle(dark);
-  header.classList.toggle(dark);
-  main.classList.toggle(dark);
-  leftSidebar.classList.toggle(dark);
-  rightSidebar.classList.toggle(dark);
-  createNewPost.classList.toggle(dark);
-  //   publishedPost.classList.toggle(dark);
-  for (i = 0; i < publishedPost.length; i++) {
-    publishedPost[i].classList.toggle(dark);
+/**  Theme Change toggler  **/
+
+const themeChangeBtn = document.querySelector(".themeBtn");
+themeChangeBtn.onclick = function (event) {
+  event.preventDefault();
+  event.stopPropagation();
+  this.classList.toggle("active");
+  body.classList.toggle("darkTheme");
+  if (localStorage.getItem("theme") === "light") {
+    localStorage.setItem("theme", "dark");
+  } else if (localStorage.getItem("theme") === "dark") {
+    localStorage.setItem("theme", "light");
   }
 };
 
-/**  End: Theme toggler  **/
+if (localStorage.getItem("theme") === "light") {
+  body.classList.remove("darkTheme");
+  themeChangeBtn.classList.remove("active");
+} else if (localStorage.getItem("theme") === "dark") {
+  body.classList.add("darkTheme");
+  themeChangeBtn.classList.add("active");
+} else {
+  localStorage.setItem("theme", "light");
+}
+
+/**  End: Theme Change toggler  **/
